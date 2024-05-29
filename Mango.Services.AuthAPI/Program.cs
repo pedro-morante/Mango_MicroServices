dd794e7bd3f26f918e76ad8da379072d861a7492
Pedro Morante <pedro.morante@inchcape.com>
2024-05-29 11:41:09 -0500

    Add Register and Loging methods

--- Mango.Services.AuthAPI/Program.cs
+++ Mango.Services.AuthAPI/Program.cs
@@ -1,21 +1,24 @@
-using AutoMapper;
-using Mango.Services.CouponAPI;
-using Mango.Services.CouponAPI.Data;
+using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Service.IService;
+using Mango.Services.AuthAPI.Service;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
-builder.Services.AddSingleton(mapper);
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+//builder.Services.AddScoped<IMessageBus, MessageBus>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,13 +33,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 ApplyMigration();
 app.Run();
-
 void ApplyMigration()
 {
     using (var scope = app.Services.CreateScope())
---
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Service.IService;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
